7dbb261c3dc98eb9e1563754e402502a09e340ed
4bitFox <33175205+4bitFox@users.noreply.github.com>
2025-03-21 10:20:00 +0100

    Update lintransform_sp.py

--- python/bigbrain/lintransform_sp.py
+++ python/bigbrain/lintransform_sp.py
@@ -9,18 +9,32 @@ Created on Fri Mar  7 11:34:42 2025
 import sympy as sp
 
 def hduplicate(vec, n):
+    """
+    returns matrix with vector repeated horizontally n times.
+    """
     M = vec * sp.ones(1, n)
     return M
 
 def coeff_matrix(P, Q):
+    """
+    Returns the coefficeint matrix of P -> Q
+    Derived from Q = A*P
+    """
     A = Q*P.inv()
     return A
 
 def transform(P, A, b = sp.zeros(2, 1)):
+    """
+    Transform P with A and b to Q
+    Q = A*P+b
+    """
     Q = A * P + b
     return Q
     
 def fixed_point(A, b):
+    """
+    Try to find a fixed point
+    """
     eye = sp.eye(A.shape[0])
     eyeA = eye - A
     eyeAb = eyeA.row_join(b)
@@ -35,15 +49,39 @@ def fixed_point(A, b):
         print("Exisiteren unendlich x_F!!!")
         return eyeAb.rref()[0]
 
-def rotatation_matrix(angle):
+def rotatation_matrix(angle, axis = None):
+    """
+    Calculate the rotation matrix with a given angle
+    if axis is not defined, 2 dimensions are assumed
+    if axis is defined as x, y, z, the matrix will be 3-dimensional
+    """
     s = sp.sin(angle)
     c = sp.cos(angle)
-    rot_M = sp.Matrix([[c, -s], [s, c]])
+    if axis == None: # 2D
+        rot_M = sp.Matrix([[c, -s], 
+                           [s,  c]])
+    elif axis == "z": # 3D around z
+        rot_M = sp.Matrix([[ c, -s,  0], 
+                           [ s,  c,  0], 
+                           [ 0,  0,  1]])
+    elif axis == "x": # 3D around x
+        rot_M = sp.Matrix([[ 1,  0,  0], 
+                           [ 0,  c, -s], 
+                           [ 0,  s,  c]])
+    elif axis == "y": # 3D around y
+        rot_M = sp.Matrix([[ c,  0,  s], 
+                           [ 0,  1,  0], 
+                           [-s,  0,  c]])
     return rot_M
 
-def rotate_2D(M, angle, rotation_point = sp.zeros(2, 1)):
+def rotate(M, angle, rotation_point = sp.zeros(2, 1), axis = None):
+    """
+    rotate M at with a given angle. 
+    If a rotation point is given, M will be rotated around it.
+    If axis isn't specified, M is assumed to be 2D. Otherwise rotation will take place around the specified axis x, y, or z and M is assumed to be 3D.
+    """
     M_amount_vectors = M.shape[1]
     if M_amount_vectors != 1: # broadcast if matrix with hstacked vectors
         rotation_point = hduplicate(rotation_point, M_amount_vectors)
-    rotated = rotatation_matrix(angle) * (M - rotation_point) + rotation_point
+    rotated = rotatation_matrix(angle, axis) * (M - rotation_point) + rotation_point
     return rotated
