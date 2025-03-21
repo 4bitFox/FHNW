dd4ac3dc92bffdbf96214148716ae3d7db639f73
4bitFox <33175205+4bitFox@users.noreply.github.com>
2025-03-21 11:01:20 +0100

    Update plot_sp.py

--- python/bigbrain/plot_sp.py
+++ python/bigbrain/plot_sp.py
@@ -13,7 +13,6 @@ def line(p1, p2 = sp.zeros(2, 1)):
     plt.plot([p1[0], p2[0]], [p1[1], p2[1]], "k-", lw=2)
     plt.grid("True")
     
-def lines_hstacked(M):
+def lines_between_hstacked_points(M):
     plt.plot( [M[0,i] for i in range(M.shape[1])], [M[1,i] for i in range(M.shape[1])], "k-", lw=2)
     plt.grid("True")
-    
