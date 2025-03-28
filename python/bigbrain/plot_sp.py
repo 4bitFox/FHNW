847e13295d5495931b85fcffc7043d35ebffc4cc
4bitFox <33175205+4bitFox@users.noreply.github.com>
2025-03-28 11:22:25 +0100

    Update plot_sp.py

--- python/bigbrain/plot_sp.py
+++ python/bigbrain/plot_sp.py
@@ -10,12 +10,17 @@ import sympy as sp
 import matplotlib.pyplot as plt
 
 def line(p1, p2 = sp.zeros(2, 1)):
-    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], "k-", lw=2)
+    plt.plot([p1[0], p2[0]], [p1[1], p2[1]])
     plt.grid(True)
     
 def lines_between_hstacked_points(M):
-    plt.plot( [M[0,i] for i in range(M.shape[1])], [M[1,i] for i in range(M.shape[1])], "k-", lw=2)
-    plt.grid(True)
+    if M.shape[0] == 2:
+        plt.plot( [M[0,i] for i in range(M.shape[1])], [M[1,i] for i in range(M.shape[1])])
+        plt.grid(True)
+    elif M.shape[0] == 3:
+        plt_3D( [M[0,i] for i in range(M.shape[1])], [M[1,i] for i in range(M.shape[1])], [M[2,i] for i in range(M.shape[1])])
+    else:
+        raise Exception("Can only plot 2D or 3D!")
     
 def plt_3D(x = None, y = None, z = None):
     fig = plt.figure()
